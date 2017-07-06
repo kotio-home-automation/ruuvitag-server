737b7c1f5d53b56655003f41307876f09b984d45
Jori Lytter <jori.lytter@gmail.com>
2017-07-06 18:18:12 +0300

    Add second ruuvitag configuration, increase ruuvitag util scan time

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const enabled = true
 
 const tags = [
-  {"name": "Työhuone", "id": "cf8cd871dcbf"}
+  {"name": "Työhuone", "id": "cf8cd871dcbf"},
+  {"name": "Makuuhuone", "id": "dcad5164dc27"}
 ]
 
 module.exports = {
